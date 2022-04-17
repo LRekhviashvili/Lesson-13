0ac6c417d7475831eaddb2b8de19edc70cab3bb5
LRekhviashvili <rekhviashvili089@gmail.com>
2022-04-17 22:28:23 +0400

    add functions

--- script.js
+++ script.js
@@ -45,12 +45,53 @@ for (let z of array1) {
     }
 }
 
-//Funcations
+//Functions
+
+function sum (...args) {
+    let sumNumbers = 0;
+    for (let item of args) {
+        if (item > 0) {
+            sumNumbers += item;
+            console.log(sumNumbers)
+        }
+    }
+}
+let answer = sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+
+function sum (...args) {
+    let sumNumbers = 0;
+    for (let item of args) {
+            sumNumbers += item;
+            console.log(sumNumbers)
+    }
+}
+
+let answer1 = sum (10, 50, 6, 7, 8, 11, 6, 3, 9);
 
 
+let user1 = {
+    firstname:"giorgi",
+    lastname: "saakadze",
+    age: 32,
+    isloggedin: true
+   }
+function myName (){
+   if (user1.isloggedin == true){
+    console.log (user1.firstname + " " + user1.lastname);
+    }else{
+        console.log ("false");
+    }
+}
+myName (user1); 
+       
+function findMax(arr) {
+    return Math.max.apply(null, arr);
+  }
+  
+  var new_arr = [45, 67, -92, 100];
+  
+  console.log(findMax(new_arr))
 
-    
-    
 
 
     
